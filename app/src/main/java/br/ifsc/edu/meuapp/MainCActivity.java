2c005897e80860556dfab90be62e0413276983e3
Isabela <isabelablu_sc@yahoo.com.br>
2020-03-05 11:27:36 -0300

    Initial commit

---
+++ app/src/main/java/br/ifsc/edu/meuapp/MainCActivity.java
@@ -0,0 +1,14 @@
+package br.ifsc.edu.meuapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainCActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activityC_main);
+    }
+}
