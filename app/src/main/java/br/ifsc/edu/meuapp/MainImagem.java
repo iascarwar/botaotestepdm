87d5cabbf43f5a6ae1389fe6bac17024b34c9ac8
Isabela <isabelablu_sc@yahoo.com.br>
2020-03-12 11:12:57 -0300

    app pra mudar foto

--- app/src/main/java/br/ifsc/edu/meuapp/MainImagem.java
+++ app/src/main/java/br/ifsc/edu/meuapp/MainImagem.java
@@ -6,19 +6,45 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MainImagem extends AppCompatActivity {
     ImageView imageView;
+    private int i=0; //posição da imagem
+    List<Integer> lista_imagem;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_imagem);
         imageView = findViewById(R.id.imageView);
-    }
-    
-    public void mudaTexto (View view){
-        imageView.setImageResource(R.drawable.cachorro);
 
+        lista_imagem = Arrays.asList(
+                R.drawable.porquinho,
+                R.drawable.patinho,
+                R.drawable.cachorro,
+                R.drawable.gardem,
+                R.drawable.happy
+        );
     }
-}
+
+    public void mudaFoto (View view){
+
+        if(i<=lista_imagem.size()-1){
+            imageView.setImageResource(lista_imagem.get(i));
+        }else {
+            i=0;
+            imageView.setImageResource(lista_imagem.get(i));
+        }
+           i++;
+        }
+
+};
+
+
+
+
+
